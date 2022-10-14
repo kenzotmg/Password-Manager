9bdc1c8d9b06a25494cab0d0eca088af9c2bcccb
kenzotmg <kenzotmg1@gmail.com>
2022-10-14 02:06:12 -0300

    fixed delete dialog bug

--- src/renderer/components/pass-card.tsx
+++ src/renderer/components/pass-card.tsx
@@ -102,6 +102,8 @@ export default function PassCard(props: { data: PasswordRecord }) {
 			setAlertSeverity(errorSeverity);
 			setOpenSnackBar(true);
 		}
+
+		setOpenDeleteDialog(false);
 	};
 
 	return (
